2c26b4348d60cee580b0e02019657f0a1ea370c2
Adam Snow <adamjohnsnow@icloud.com>
2017-04-21 13:27:06 +0100

    refactored, tests not working

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,7 +2,7 @@
 class Journey
   attr_reader :start_station, :end_station
 
-  def start_station(station)
+  def start_journey(station)
     @start_station = { entrytime: Time.new.round, entrystation: station.name, entryzone: station.zone }
   end
 
